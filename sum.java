1751ae4f7ac9c41e5d9b9f6328335c754cf0c24a
gagan-git566 <gaganclg202527@gmail.com>
2026-02-16 10:16:23 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -1,1 +1,13 @@
 
+import java.util.Scanner;
+class sum
+  {
+    public static void main(String args[])
+    {
+      Scanner S= new Scanner (System.in);
+      int a =S.nextInt();
+      int b =S.nextInt();
+      int sum = a+b;
+      System.out.println("the sum is"+sum);
+    }
+  }    
